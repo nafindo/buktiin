6b959d5f22a947d55ecaa1b55cd1083ea6e5a1c5
nafindo <ajt.nafi1@gmail.com>
2026-07-04 13:18:12 +0700

    Change logo image format from JPG to PNG

--- app/src/layouts/MainLayout.tsx
+++ app/src/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Outlet, Link, useLocation, useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabase';
-import logoImg from '../assets/images/logo.jpg';
+import logoImg from '../assets/images/logo.png';
 
 export default function MainLayout() {
   const location = useLocation();
